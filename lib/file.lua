45fa2b6b0f1bc673c4b655df4a18a03f0335af1f
mzyzc <mzyzc@czyz.xyz>
2021-05-02 12:49:07 +0100

    Added functionality for joining file paths/names

--- lib/file.lua
+++ lib/file.lua
@@ -1,7 +1,7 @@
 -- Check if a file is marked as hidden
 function isHidden (filename) return string.sub (filename, 1, 1) ~= '.' end
 
--- Split the filename into its basename and extensions, using . as a delimiter
+-- Split a string at delimiters
 local function splitString (string, delimiter)
     local stringSegments = {}
 
@@ -15,3 +15,22 @@ end
 
 function splitFilePath (filePath) return splitString (filePath, '/') end
 function splitFileName (filename) return splitString (filename, '.') end
+
+-- Join a string at delimiters
+local function joinString (string, delimiter)
+    local stringBuilder = string[1]
+
+    for i = 2, #string do
+        stringBuilder = stringBuilder..delimiter..string[i]
+    end
+
+    return stringBuilder
+end
+
+function joinFilePath (filePath)
+    -- Join the filename into a string first
+    filePath[#filePath] = joinFileName (filePath[#filePath])
+    return joinString (filePath, '/')
+end
+
+function joinFileName (filename) return joinString (filename, '.') end
