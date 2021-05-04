1ce096c4148b2b299f3d81913bf5e5af4a972ac0
mzyzc <mzyzc@czyz.xyz>
2021-05-04 22:22:36 +0100

    Improved documentation and style compliance

--- silkmason.py
+++ silkmason.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
 
-import toml
+"""Converts a collection of files into a static website"""
+
 from multiprocessing import Process
 from os import system
 from pathlib import Path
 from sys import argv
 from shutil import copy
 
+import toml
+
 def handle_directory(in_dir, out_dir):
-    """Explore a directory for files to convert"""
+    """Explore a directory recursively to look for files"""
     for in_file in in_dir.iterdir():
         # Ignore hidden files
         if in_file.name[0] == '.':
             return
 
         if in_file.is_dir():
+            # Create empty directory in the output location
             out_file = Path(out_dir, in_file.relative_to(in_dir))
             out_file.mkdir(exist_ok=True)
+
             # Explore further directories recursively
             process = Process(target=handle_directory, args=(in_file, out_file))
             process.start()
@@ -24,6 +29,7 @@ def handle_directory(in_dir, out_dir):
             handle_file(in_file, out_dir)
 
 def handle_file(in_file, out_dir):
+    """Handle files based on their extension"""
     suffix = in_file.suffix.lower()
 
     if suffix == '.md':
@@ -36,10 +42,8 @@ def handle_file(in_file, out_dir):
     print(out_file)
 
 
-# Load config
+# Load config and override some settings with command-line arguments
 config = toml.load('config.toml')
-
-# Set defaults
 config['input_dir'] = Path(argv[1] if len(argv) >= 2 else config['input_dir']).expanduser()
 config['output_dir'] = Path(argv[2] if len(argv) >= 3 else config['output_dir']).expanduser()
 
