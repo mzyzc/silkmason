677fb99e605c63dfbed78ca91b6977c7b052ddf4
mzyzc <mzyzc@czyz.xyz>
2021-05-09 15:47:02 +0100

    Added filter to automatically set title based on top-level heading

---
+++ filters/set_title.lua
@@ -0,0 +1,16 @@
+-- Set the document title to be the same as the top-level heading
+
+local title
+
+function get_title (header)
+    if not title and header.level == 1 then
+        title = header.content
+    end
+    return nil
+end
+
+return {
+    {Meta = function (meta) title = meta.title end},
+    {Header = get_title},
+    {Meta = function (meta) meta.title = title; return meta end},
+}
