624aeb9bd1e6931d63c7f4a5d7f32d3b76bd7a3f
mzyzc <mzyzc@czyz.xyz>
2021-06-01 15:00:21 +0100

    Rewrote FeedMason to be cleaner

--- feedmason.py
+++ feedmason.py
@@ -8,57 +8,64 @@ import lxml
 import toml
 
 def add_tag(node, label, data, attributes):
-    """Create a new tag and append it to a node"""
+    """Create a new XML tag"""
     soup = BeautifulSoup(features='xml')
-
-    # Set tag label
     tag = soup.new_tag(label)
 
-    # Set tag data
     if data:
-        tag.string = data
+        tag.string = str(data)
 
-    # Set tag attributes
     for attr in attributes.keys():
         tag[attr] = attributes[attr]
 
     node.append(tag)
 
-def generate_feed(feeds, root, domain, author):
-    """Generate one or more Atom feeds"""
+def create_entry(path, root, domain):
+    """Create an entry for an Atom feed"""
+    soup = BeautifulSoup(features='xml')
+    entry = root / path
+
+    if not entry.is_file():
+        return
+
+    with open(entry, 'r') as entry_file:
+        html = BeautifulSoup(entry_file, 'html.parser')
+        title = html.find('h1').string
+        summary = html.main.p
+
+    node = soup.new_tag('entry')
+    add_tag(node, 'id', domain/path, {})
+    add_tag(node, 'title', title, {})
+    add_tag(node, 'link', '', {'href': f'https://{domain/path}'})
+    #add_tag(node, 'summary', summary, {})
+
+    return node
+
+def create_feed(path, root, domain, author):
+    """Create an Atom feed"""
     soup = BeautifulSoup(features='xml')
 
-    # node, label, data, attributes
-    # Create root node
-    add_tag(soup, 'feed', '', {'xmlns': 'http://www.w3.org/2005/Atom'})
+    with open(f'{root/path}.html', 'r') as page_file:
+        html = BeautifulSoup(page_file, 'html.parser')
+        title = html.title.string
 
-    # Set global feed data
-    add_tag(soup.feed, 'id', domain, {})
-    add_tag(soup.feed, 'title', domain, {})
+    add_tag(soup, 'feed', '', {'xmlns': 'http://www.w3.org/2005/Atom'})
+    add_tag(soup.feed, 'id', domain/path, {})
+    add_tag(soup.feed, 'title', title, {})
     add_tag(soup.feed, 'author', author, {})
     add_tag(soup.feed, 'link', '', {'href': f'https://{domain}/feed'})
 
-    # Create entry nodes
-    for feed in feeds:
-        feed = root / feed  # Get absolute path of entry
-
-        for entry in feed.iterdir():
-            if not entry.is_file():
-                continue
-
-            with open(entry, 'r') as entry_file:
-                html = BeautifulSoup(entry_file, 'html.parser')
-                title = html.find('h1').string
-                summary = html.main.p
-
-                # Set entry-specific data
-                node = soup.new_tag('entry')
-                add_tag(node, 'id', f'{domain}/{entry.relative_to(root)}', {})
-                add_tag(node, 'title', title, {})
-                add_tag(node, 'link', '', {'href': f'https://{domain}/{entry.relative_to(root)}'})
-            soup.feed.append(node)
+    for entry_file in (root/path).iterdir():
+        entry = create_entry(entry_file.relative_to(root), root, domain)
+        if entry:
+            soup.feed.append(entry)
 
     return soup
+
+def combine_feeds(feeds, domain):
+    """Combine multiple Atom feeds into one"""
+    # TODO
+    pass
 
 
 # Load configuration file
@@ -68,11 +75,6 @@ author = config['feedmason']['author']
 root = Path(config['feedmason']['root']).expanduser()
 feeds = [Path(feed) for feed in config['feedmason']['feeds']]
 
-# Create combined feed
-feed = generate_feed(feeds, root, domain, author)
-print(feed, end='\n\n')
-
-# Create individual feeds
 for feed_path in feeds:
-    feed = generate_feed([feed_path], root, domain, author)
+    feed = create_feed(feed_path, root, domain, author)
     print(feed, end='\n\n')
