d004695ab48399f8535140acd06ab9a8e0403abf
mzyzc <mzyzc@czyz.xyz>
2021-07-21 10:49:51 +0100

    Added filter to convert an ISO 8601 date into a nicer format

---
+++ filters/format_date.lua
@@ -0,0 +1,16 @@
+-- Take the 'date' metadata and format it in a friendlier form for displaying
+
+function Meta(meta)
+  if meta.date then
+
+    -- Split date string into year, month, and day
+    local date = {}
+    for unit in string.gmatch(meta.date[1].text, "([^"..'-'.."]+)") do
+            table.insert(date, unit)
+    end
+
+    date = os.time ({year = date[1], month = date[2], day = date[3]})
+    meta.datef = os.date ("%e %B %Y", date)
+    return meta
+  end
+end
