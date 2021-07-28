8e856f5a1ce8dcf68eb1c11ab193fe7d15e460cb
mzyzc <mzyzc@czyz.xyz>
2021-07-28 15:39:28 +0100

    Added automatic description-setting filter and included some filters in the example configuration file

---
+++ filters/set_description.lua
@@ -0,0 +1,16 @@
+-- Set the document description to the first paragraph
+
+local description
+
+function get_description (paragraph)
+    if not description then
+        description = paragraph.content
+    end
+    return nil
+end
+
+return {
+    {Meta = function (meta) description = meta.description end},
+    {Para = get_description},
+    {Meta = function (meta) meta.description = description; return meta end},
+}
