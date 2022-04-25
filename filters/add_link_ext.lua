8e2dca9193ae486bb100d8858a65c39e4a71bfc5
mzyzc <mzyzc@czyz.xyz>
2022-04-25 12:24:57 +0100

    Extension-adding filter no longer adds an extension if the path ends in '/' (for index pages)

--- filters/add_link_ext.lua
+++ filters/add_link_ext.lua
@@ -1,11 +1,11 @@
 -- Add a HTML file extension to links with no extension
 
 function Link(link)
+    if link.target:sub (-1, -1) == "/" then return end
+
     local ext = link.target:match ("%.(.*)$")
+    if ext then return end
 
-    if not ext then
-        link.target = link.target .. ".html"
-    end
-
+    link.target = link.target .. ".html"
     return link
 end
