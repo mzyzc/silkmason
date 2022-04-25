f6707f05b5b1e9da502f260f97fd0aaa29defef8
mzyzc <mzyzc@czyz.xyz>
2022-04-25 11:21:40 +0100

    Added filter to add HTML extension to links without an extension

---
+++ filters/add_link_ext.lua
@@ -0,0 +1,11 @@
+-- Add a HTML file extension to links with no extension
+
+function Link(link)
+    local ext = link.target:match "%.(.*)$"
+
+    if not ext then
+        link.target = link.target..".html"
+    end
+
+    return link
+end
